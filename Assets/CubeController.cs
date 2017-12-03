f24ea36f822797a7f2da070ca3cb6cb2c42ae878
yuan31810 <yuan31810@gmail.com>
2017-12-03 17:05:11 +0900

    Lesson7　2Dのゲーム
    ブロックが落ちる音は半分くらいにボリュームダウンしています

---
+++ Assets/CubeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CubeController : MonoBehaviour {
+    private float speed = -0.2f;
+    private float deadline = -10;
+    private AudioSource audioSource;
+    public AudioClip audioClip;
+	// Use this for initialization
+	void Start () {
+        audioSource =gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(this.speed, 0, 0);
+        if (transform.position.x < this.deadline)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "CubeTag")
+        {
+            audioSource.Play();
+        }
+    }
+}
